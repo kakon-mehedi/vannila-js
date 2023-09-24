04dda4991913fdbe3a52286e54a4a3ce7bc08f55
kakon-selise <kakon.mehedi@selise.ch>
2023-09-24 18:40:35 +0600

    Learning this

---
+++ script.js
@@ -0,0 +1,51 @@
+function generalLevelFn() {
+	console.log(this); // window
+}
+
+function nestedLevelThisFn() {
+	nested1();
+	nested2();
+}
+
+function nested1() {
+	console.log(this); // window
+}
+
+function nested2() {
+	console.log(this); // window
+}
+
+const userDetails = {
+	name: 'Kakon',
+	age: 20,
+	consoleRegularFn: function () {
+		console.dir(this); // userDetailsObject
+	},
+
+	consoleArrowFn: () => {
+		console.log(this); // window
+	},
+};
+
+const thisArrowFn = () => {
+	console.log(this);
+};
+
+generalLevelFn();
+nestedLevelThisFn();
+userDetails.consoleRegularFn();
+userDetails.consoleArrowFn();
+thisArrowFn();
+
+/* Learning
+    
+    This inside
+    
+    Normal Function                   = window
+    Normal Functions inside an object = current Object 
+    Inside an object                  = current Object
+
+    Arrow Function                    = window
+    Arrow Function inside object      =  
+
+*/
