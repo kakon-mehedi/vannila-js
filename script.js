c83949e2b64560008522d57018ffc8fe66c8c589
kakon-selise <kakon.mehedi@selise.ch>
2023-10-06 20:43:50 +0600

    More reducer

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const arr = [1, 2, 3, 4];
-const result = arr.reduce(printSumCallbackFn);
+//const result = arr.reduce(printSumCallbackFn);
 
 /**
  *
- * @param {*} previousValue - previous value of the array
- * @param {*} currentValue - current value of the array
- * @param {*} currentIndex - current index of the element
+ * @param {*} previousValue - previous value of the array, this is the previous return value of the callback function
+ * @param {*} currentValue - current value of the array, always the current value of the array
+ * @param {*} currentIndex - current index of the element, current value index
  * @param {current inputed array} arr
  * @returns - this will return the sum of previous value and the current value.
  * You can return anything may be you make an array and return the array
@@ -21,4 +21,21 @@ function printSumCallbackFn(previousValue, currentValue, currentIndex, arr) {
 	return previousValue + currentValue;
 }
 
-console.log(result);
+const numberList = [1, 2, 3, 4];
+const sumList = [];
+
+const resultSum = numberList.reduce(sumOfElements);
+
+/**
+ * This function will modify sumList Array and put numberList sum of the consecutive array element.
+ * @param {p} prev - This is the previous value of the array
+ * @param {*} current - current number
+ * @param {*} currentIndex - index of the current number
+ * @param {*} currentArr - current array
+ */
+function sumOfElements(prev, current, currentIndex, currentArr) {
+	const newValue = currentArr[currentIndex - 1] + current;
+	sumList.push(newValue);
+}
+
+console.log(sumList); // 3.5.7
